590d314d447741a246493c53d604acbac93e17ee
Maxim Boldyrev <maxrox1904@gmail.com>
2024-05-31 18:46:04 +0300

    add_favorites.favorites_sprt, get_f_ auth

---
+++ backend/app/add_favorites.php
@@ -0,0 +1,37 @@
+<?php
+// Настройки подключения к базе данных
+$dbhost = 'postgres-db';
+$dbname = 'obu-hack-2024';
+$dbuser = 'user';
+$dbpass = 'user';
+
+try {
+    $pdo = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]);
+} catch (\PDOException $e) {
+    echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
+    exit;
+}
+
+// Получение данных из POST-запроса
+$id_place = $_POST['id_place'] ?? null;
+$id_user = $_POST['id_user'] ?? null;
+
+// Проверка, что данные были переданы
+if ($id_place && $id_user) {
+    // Подготовка и выполнение SQL-запроса
+    $sql = "INSERT INTO favorites (id_place, id_user) VALUES (:id_place, :id_user)";
+    $stmt = $pdo->prepare($sql);
+
+    try {
+        $stmt->execute(['id_place' => $id_place, 'id_user' => $id_user]);
+        echo json_encode(['status' => 'success', 'message' => 'Favorite added successfully']);
+    } catch (\PDOException $e) {
+        echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
+    }
+} else {
+    echo json_encode(['status' => 'error', 'message' => 'Missing id_place or id_user']);
+}
--- backend/app/auth.php
+++ backend/app/auth.php
@@ -14,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $password = $_POST["password"];
 
         $stmt = $pdo->prepare("SELECT * FROM users WHERE email_user = ? AND password_user = ?");
-        $stmt->execute([$email, hash('sha256', $password)]);
+        $stmt->execute([$email,  $password]);
         $user = $stmt->fetch();
 
         if ($user) {

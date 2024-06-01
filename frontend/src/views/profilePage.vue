d5502b0210a3cc51a4ae232d12257304a8ae851d
Vladimir Tolstunov <vovaone2003@gmail.com>
2024-06-01 12:13:49 +0300

    Changes in profile page

--- frontend/src/views/profilePage.vue
+++ frontend/src/views/profilePage.vue
@@ -1,24 +1,31 @@
 <template>
-  <div>
-    <header />
+  <div class="flex flex-col gap-8">
+    <Header />
     <main class="flex gap-8">
-      <nav class="flex flex-row">
-        <Button variant="ghost" class="flex gap-2"><Heart /> Избранное</Button>
-        <Button variant="ghost" class="flex gap-2"><History /> История</Button>
-        <Button variant="ghost" class="flex gap-2"><Settings /> Настройки</Button>
-      </nav>
+      <Card>
+        <nav class="flex flex-col gap-1">
+          <Button class="flex gap-2 justify-start w-[320px]"><Heart /> Избранное</Button>
+          <Button variant="ghost" class="flex gap-2 justify-start w-[320px]"
+            ><History /> История</Button
+          >
+          <Button variant="ghost" class="flex gap-2 justify-start w-[320px]"
+            ><Settings /> Настройки</Button
+          >
+        </nav>
+      </Card>
+      <section>
+        <h3>Всего {{ placesAmount }} мест</h3>
+      </section>
     </main>
-    <section>
-      <h3>Всего {{ placesAmount }} мест</h3>
-    </section>
   </div>
 </template>
 
 <script setup>
-import header from '@/components/custom/profile/header.vue'
-import Button from '@/components/ui/button'
+import Header from '@/components/custom/profile/header.vue'
+import { Button } from '@/components/ui/button'
 
 import { Heart, History, Settings } from 'lucide-vue-next'
+import { Card } from '@/components/ui/card'
 
 import { ref } from 'vue'
 

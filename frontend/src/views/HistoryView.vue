5294c50fba9900f72f513d9d2bb2a94378097863
Vladimir Tolstunov <vovaone2003@gmail.com>
2024-06-01 16:37:32 +0300

    Complete history

--- frontend/src/views/HistoryView.vue
+++ frontend/src/views/HistoryView.vue
@@ -1,7 +1,49 @@
 <template>
-  <div>
-    <p>history view</p>
-  </div>
+  <section class="flex flex-col gap-6 w-full">
+    <div class="flex flex-col gap-2">
+      <h3>Вы искали раньше</h3>
+      <p>
+        Нажмите на строку в таблице, чтобы вернуться в то прекрасное время, когда вы делали этот
+        запрос.
+      </p>
+    </div>
+    <div class="flex flex-col">
+      <Table class="w-full">
+        <TableHeader>
+          <TableRow>
+            <TableHead class="font-bold">Откуда</TableHead>
+            <TableHead class="font-bold">Куда</TableHead>
+            <TableHead class="font-bold">Дата вылета </TableHead>
+            <TableHead class="font-bold">Дата просмотра</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody v-for="item in historyData" :key="item[0]">
+          <TableRow>
+            <TableCell>{{ item[1] }}</TableCell>
+            <TableCell>{{ item[2] }}</TableCell>
+            <TableCell>{{ item[3] }}</TableCell>
+            <TableCell>{{ item[4] }}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  </section>
 </template>
 
-<script setup></script>
+<script setup>
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table'
+
+import { ref } from 'vue'
+
+const historyData = ref([
+  [1, 'Липецк', 'Санкт-Петербург', '20.10.2023', '10.10.2023'],
+  [2, 'Москва', 'Самара', '10.10.2022', '05.10.2022']
+])
+</script>
